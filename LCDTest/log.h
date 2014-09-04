208a96dc80ca1ac96e7cc15098a7cc3aa2ce4ab4
KART35 <netbug@cox.net>
2014-09-04 11:19:53 -0700

    documentation

--- LCDTest/log.h
+++ LCDTest/log.h
@@ -6,29 +6,36 @@
 #include <strstream>
 #include <fstream>
 
+//! macro for logSys::putMessage. Usage: logSys::putMessage(LOGINFO(long))
 #define LOGINFO(X) logSys::errInfo(__LINE__, __FUNCTION__, __FILE__, X )
+
+
 namespace logSys{
-	typedef enum severity {
-		debug,
-		info,
-		warn,
-		error
+	
+	//! used to define how pear shaped things are
+	typedef enum severity 
+	{
+		debug, //!< debugging messages. these should be disabled in release versions
+		info, //!< info messages, not as verbose as debug
+		warn, //! a recoverable error
+		error //! a fatal error.
 	};
 
 	struct eInf_
 	{
-		unsigned int line = 0;
-		long errCode = 0;
-		std::string cName;
-		std::string fName;
+		unsigned int line = 0; //!< line the call to putMessage is from
+		long errCode = 0; //!< error code, or other useful info
+		std::string cName; //!< class or function name
+		std::string fName; //!< file name
 	};
-
+	
+	//! an easy to create data structure for error messages
 	class errInfo
 	{
 	protected:
 		eInf_ info;
 	public:
-
+		
 		errInfo(int ln, std::string clName, std::string fileName, long eCode)
 		{
 			this->info.line = ln;
@@ -41,7 +48,7 @@ namespace logSys{
 			return this->info;
 		}
 	};
-
+	//! prints info in arguments to a log file with a timestamp
 	void putMessage(std::string mess, severity sev, errInfo info);
 
 }
